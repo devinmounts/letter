b134065a7d7b22540d963fc4492a8e9793c096eb
Brandon Betts and Devin Mounts <devin@>
2018-05-30 16:23:02 -0700

    help me learn syntax.

--- js/scripts.js
+++ js/scripts.js
@@ -1,19 +1,36 @@
-$(document).ready(function() {
-  $("#formOne").submit(function(event) {
-    var nameInput = $("input#name").val();
-    var addressInput = $("input#address").val();
-    var toInput= $("input#to").val();
-    var exclamationInput = $("input#exclamation").val();
-    var closingInput = $("input#closing").val();
+// $(document).ready(function() {
+//   $("#formOne").submit(function(event) {
+//     var nameInput = $("input#name").val();
+//     var addressInput = $("input#address").val();
+//     var toInput= $("input#to").val();
+//     var exclamationInput = $("input#exclamation").val();
+//     var closingInput = $("input#closing").val();
+//
+//     $(".name").text(nameInput);
+//     $(".address").text(addressInput);
+//     $(".to").text(toInput);
+//     $(".exclamation").text(exclamationInput);
+//     $(".closing").text(closingInput);
+//
+//     $("#letter").show();
+//
+//     event.preventDefault();
+//   });
+// });
 
-    $(".name").text(nameInput);
-    $(".address").text(addressInput);
-    $(".to").text(toInput);
-    $(".exclamation").text(exclamationInput);
-    $(".closing").text(closingInput);
+//
+$(document).ready(function(){
+  ("formOne").submit(function(event){
+    var name = $("#name").val();
+    var address = $("#address").val();
+    var to = $("#to").val();
+    var exclamation = $("#to").val();
+    var closing = $("#closing").val();
 
-    $("#letter").show();
-
-    event.preventDefault();
-  });
-});
+    $(".name").text(name);
+    $(".address").text(address);
+    $(".to").text(to);
+    $("exclamation").text(exclamation);
+    $("closing").text(closing);
+  })
+})
